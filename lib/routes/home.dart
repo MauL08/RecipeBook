f366c45def8b3b4259a1257a89a5ab64ce7ead43
MosQyy <akbarma2002@gmail.com>
2021-06-08 01:35:16 +0700

    finishhh

--- lib/routes/home.dart
+++ lib/routes/home.dart
@@ -2,50 +2,132 @@ import 'package:flutter/material.dart';
 import 'package:recipe_book/controller/optionmenu_controller.dart';
 import 'package:recipe_book/controller/slideshow_controller.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.green,
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text(
-          "Buku Resep Nusantara",
-          style: TextStyle(fontWeight: FontWeight.bold),
-        ),
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-              padding: EdgeInsets.all(20.0),
-              margin: EdgeInsets.only(top: 28.0),
-              decoration: BoxDecoration(
-                color: Colors.orange,
-                shape: BoxShape.rectangle,
-                borderRadius: BorderRadius.circular(12.0),
-              ),
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  Future<bool> _exitApp() async {
+    return await showDialog(
+      context: context,
+      builder: (context) {
+        return Dialog(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(
+              Radius.circular(10.0),
+            ),
+          ),
+          elevation: 0,
+          backgroundColor: Colors.transparent,
+          child: Container(
+            width: 400,
+            height: 200,
+            decoration: BoxDecoration(
+              color: Colors.orange,
+              shape: BoxShape.rectangle,
+              borderRadius: BorderRadius.circular(24.0),
+              border: Border.all(color: Colors.black, width: 5.0),
+            ),
+            child: Center(
               child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   Text(
-                    "Selamat Datang di",
+                    "Keluar Aplikasi?",
                     style: TextStyle(
-                      fontSize: 20.0,
                       fontWeight: FontWeight.bold,
-                      color: Colors.black,
+                      fontSize: 18.0,
                     ),
                   ),
-                  Image.asset("images/logo/logo.png")
+                  SizedBox(height: 30.0),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      FlatButton(
+                        hoverColor: Colors.red,
+                        child: Text(
+                          "Tidak",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 15.0,
+                          ),
+                        ),
+                        onPressed: () {
+                          Navigator.pop(context, false);
+                        },
+                      ),
+                      RaisedButton(
+                        color: Colors.orange[800],
+                        hoverColor: Colors.red,
+                        child: Text(
+                          "Ya",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 15.0,
+                          ),
+                        ),
+                        onPressed: () {
+                          Navigator.pop(context, true);
+                        },
+                      )
+                    ],
+                  )
                 ],
               ),
             ),
-            SlideHomePreview(),
-            MenuController(),
-            SizedBox(height: 10.0)
-          ],
+          ),
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return WillPopScope(
+      child: Scaffold(
+        backgroundColor: Colors.green,
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text(
+            "Buku Resep Nusantara",
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                padding: EdgeInsets.all(20.0),
+                margin: EdgeInsets.only(top: 28.0),
+                decoration: BoxDecoration(
+                  color: Colors.orange,
+                  shape: BoxShape.rectangle,
+                  borderRadius: BorderRadius.circular(12.0),
+                ),
+                child: Column(
+                  children: [
+                    Text(
+                      "Selamat Datang di",
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                      ),
+                    ),
+                    Image.asset("images/logo/logo.png")
+                  ],
+                ),
+              ),
+              SlideHomePreview(),
+              MenuController(),
+              SizedBox(height: 10.0)
+            ],
+          ),
         ),
       ),
+      onWillPop: _exitApp,
     );
   }
 }
